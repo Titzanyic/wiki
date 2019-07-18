8904b1d24c6aab8ca442fd9d824e30fbe6b4d41b
titzanyic <zhangyongcun521@gmail.com>
2019-07-18 13:07:09 +0800

    add sh hidden and show for dsm

---
+++ dsm/hidden.sh
@@ -0,0 +1,11 @@
+cp /usr/syno/synoman/webman/modules/DSMNotify/style.css /usr/syno/synoman/webman/modules/DSMNotify/style.css.bak
+cd /usr/syno/synoman/webman/modules/DSMNotify/
+curl -LO https://raw.githubusercontent.com/titzanyic/wiki/master/dsm/style.css
+
+cp /usr/syno/synoman/webman/modules/AdminCenter/style.css /usr/syno/synoman/webman/modules/AdminCenter/style.css.bak
+cd /usr/syno/synoman/webman/modules/AdminCenter/
+curl -LO https://raw.githubusercontent.com/titzanyic/wiki/master/dsm/AdminCenter/style.css
+
+cp /usr/syno/synoman/synoSDSjslib/sds.css /usr/syno/synoman/synoSDSjslib/sds.css.bak
+cd /usr/syno/synoman/synoSDSjslib/
+curl -LO https://raw.githubusercontent.com/titzanyic/wiki/master/dsm/sds.css
---
+++ dsm/show.sh
@@ -0,0 +1,3 @@
+mv /usr/syno/synoman/synoSDSjslib/sds.css.bak /usr/syno/synoman/synoSDSjslib/sds.css
+mv /usr/syno/synoman/webman/modules/AdminCenter/style.css.bak /usr/syno/synoman/webman/modules/AdminCenter/style.css
+mv /usr/syno/synoman/webman/modules/DSMNotify/style.css.bak /usr/syno/synoman/webman/modules/DSMNotify/style.css
