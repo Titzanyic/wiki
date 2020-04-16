70718e6bea0e52223b3283543b5543ce792d7bc4
zhangyongcun <zhang.yongcun@ny.renderg.com>
2020-04-16 13:16:52 +0800

    update tide js

--- surge/tide.js
+++ surge/tide.js
@@ -3,5 +3,8 @@ Surge & QX MITM = tide-api.moreless.io
 */
 
 let obj=JSON.parse($response.body)
-obj.account["vip"]["is_lifetime_member"] = true;
+obj.vip["is_lifetime_member"] = true;
+obj.vip["expired_at"] = 1585691786;
+obj.vip["updated_at"] = 1585621786;
+obj.vip["is_lifetime_member"] = 1585621786;
 $done({body:JSON.stringify(obj)})
